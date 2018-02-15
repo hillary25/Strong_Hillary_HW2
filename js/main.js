2c6e9fa072515f021dc30e30c521ad91624dce61
Hillary <h_strong69707@fanshaweonline.ca>
2018-02-15 10:40:17 -0500

    in-class final changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 (() => {
   console.log('fired!');
+
+  //variables always go at the top
+  var iconSet = document.querySelectorAll('svg');
+
+  //functions here
+  function logThisIcon(){
+    console.log('click on this icon: ', this.id);
+
+    //change opacity
+    this.style.opacity = 0.5;
+
+  }
+
+
+  //event handling goes at the bottom
+  iconSet.forEach(icon => icon.addEventListener('click',logThisIcon));
 })();
